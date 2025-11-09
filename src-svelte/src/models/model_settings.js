e2fe7f3ae43a33708ac823b924ba11662621a81b
Ian Scrivener <163229+ianscrivener@users.noreply.github.com>
2025-11-10 09:43:15 +1100

    create archive branch

--- src-svelte/src/models/model_settings.js
+++
@@ -1,8 +0,0 @@
-
-export const settings = {
-    DT_BASE_DIR: "/Users/user/Library/Containers/com.liuliu.draw-things/Data/Documents",
-    STASH_DIR: "/Volumes/Extreme2Tb/__DrawThings_Stash__",
-    DTC_APP_DIR: "/Users/user/.drawthings_companion",
-    initialized: true,
-    initialized_date: "2025-01-01T00:00:00Z",
-};
